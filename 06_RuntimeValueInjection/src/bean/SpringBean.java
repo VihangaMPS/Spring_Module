170156db5495ebdad25b3aea000b23e07f3621c4
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-07 14:59:29 +0530

    Updated Content

--- 06_RuntimeValueInjection/src/bean/SpringBean.java
+++ 06_RuntimeValueInjection/src/bean/SpringBean.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean(){
+
+    public SpringBean() {
+        System.out.println("Spring bean Default Constructor");
+    }
+
+    public SpringBean(String id){
         System.out.println("Spring Bean Instantiated");
     }
 }
