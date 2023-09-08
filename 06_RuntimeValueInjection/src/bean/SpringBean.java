b252fcbbf97f446e96720211c72816182993771f
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-08 12:29:15 +0530

    intro @Autowide

--- 06_RuntimeValueInjection/src/bean/SpringBean.java
+++ 06_RuntimeValueInjection/src/bean/SpringBean.java
@@ -13,6 +13,27 @@ public class SpringBean {
             System.out.println("Spring bean Default Constructor");
         }
     */
+
+  /*  @Autowired(required = false)
+    public SpringBean(@Value("C001") String id,
+                      @Value("true") boolean b){
+        System.out.println("Spring Bean Instantiated");
+        System.out.println(id);
+        System.out.println(b);
+    }
+*/
+    @Autowired(required = false)// greediest constructor ( the highest parameter count )
+    public SpringBean(@Value("1,2,3") int [] myNames,@Value("A")char a){
+        System.out.println("Spring Bean Instantiated");
+
+        for (Integer myName: myNames) {
+            System.out.println(myName);
+        }
+        System.out.println(a);
+    }
+
+
+
     @Autowired(required = false)
     public SpringBean(@Value("C001") String id,
                       @Value("10") int age,
@@ -21,15 +42,5 @@ public class SpringBean {
         System.out.println(id);
         System.out.println(age);
         System.out.println(b);
-    }
-
-    @Autowired(required = false)
-    public SpringBean(@Value("1,2,3") int [] myNames,@Value("A")char a){
-        System.out.println("Spring Bean Instantiated");
-
-        for (Integer myName: myNames) {
-            System.out.println(myName);
-        }
-        System.out.println(a);
     }
 }
