85b3923977a34ef340d8b81919bde61fb32c4ee8
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-08 12:48:47 +0530

    intro @Autowide

--- 06_RuntimeValueInjection/src/bean/SpringBeanTwo.java
+++ 06_RuntimeValueInjection/src/bean/SpringBeanTwo.java
@@ -1,10 +1,11 @@
 package bean;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeanTwo {
+public class SpringBeanTwo implements InitializingBean {
     @Value("C001")
     private String id;
     @Value("Vishnuka Yahan")
@@ -17,6 +18,15 @@ public class SpringBeanTwo {
 
     public SpringBeanTwo() {
         System.out.println("Spring Bean Two Instantiated");
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(salary);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("=================================================");
         System.out.println(id);
         System.out.println(name);
         System.out.println(address);
