4375d6ff03869135acfb85c59d88166f928f6efe
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-08 23:05:29 +0530

    add Spring Envirement

--- 07_SpringEnvirement/src/AppInitializer.java
+++ 07_SpringEnvirement/src/AppInitializer.java
@@ -1,11 +1,22 @@
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
         context.refresh();
         context.registerShutdownHook();
+
+   //  Access System Environment Variable With Java
+   //   System.getenv();
+
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            System.out.println(key+ " : "+getenv.get(key));
+
+        }
     }
 }
