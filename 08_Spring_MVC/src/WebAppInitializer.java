86a6233f8604d459644db575b45b2966a9ad2cf5
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-10 17:53:02 +0530

    spring web mvc

--- 08_Spring_MVC/src/WebAppInitializer.java
+++ 08_Spring_MVC/src/WebAppInitializer.java
@@ -1,2 +1,19 @@
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
