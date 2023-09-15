5d85db4e3f98ddfc4f227b68cd22e9aadf2a0aee
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-15 16:40:58 +0530

    Updated Content

--- 09_RestControllers/src/WebAppInitializer.java
+++ 09_RestControllers/src/WebAppInitializer.java
@@ -1,5 +1,21 @@
-public class WebAppInitializer {
-    public static void main(String[] args) {
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
     }
 }
---
+++ 09_RestControllers/src/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class WebRootConfig {
+}
