be06b81861765c1fc381e9b9836f478e8dfee438
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-16 17:44:55 +0530

    Path variable intro

--- 10_RestController/src/main/java/lk/ijse/spring/controller/PathVariableController.java
+++ 10_RestController/src/main/java/lk/ijse/spring/controller/PathVariableController.java
@@ -9,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("variable")
 public class PathVariableController {
 
+    //http://localhost:8000/restcontroller/variable/I001
+    //(variable) path segment
+    //(I001) path segment
+
+    //in spring, we can retrieve value of path segment
+    // to do that we have to creat path variables -> {I001}
+
+
     @GetMapping(path = "{I001}")
     public String test(@PathVariable("I001") String  itemCode){
         return itemCode;
