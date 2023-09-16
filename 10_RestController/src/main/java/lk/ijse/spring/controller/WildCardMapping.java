6e53d3c2a90f115c764cc05063e992c896bf1c12
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-16 17:04:27 +0530

    wild card mapping

--- 10_RestController/src/main/java/lk/ijse/spring/controller/WildCardMapping.java
+++ 10_RestController/src/main/java/lk/ijse/spring/controller/WildCardMapping.java
@@ -1,10 +1,19 @@
 package lk.ijse.spring.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("wildcard")
+@RequestMapping("one")
 public class WildCardMapping {
+
+
+
+    @GetMapping(path = "test/*/hello")
+    public String test(){
+        return "Get Mapping Invoked (test) ";
+    }
+
 }
