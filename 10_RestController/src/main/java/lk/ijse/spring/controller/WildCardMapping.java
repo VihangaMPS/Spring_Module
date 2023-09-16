1b46c60723a956b56601a5a35b0133899dc3ba34
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-16 17:18:22 +0530

    Updated Content

--- 10_RestController/src/main/java/lk/ijse/spring/controller/WildCardMapping.java
+++ 10_RestController/src/main/java/lk/ijse/spring/controller/WildCardMapping.java
@@ -10,10 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class WildCardMapping {
 
 
+    // wild card mapping
+    //Matches one or more Character in a path segment
+    //test/*/hello -->  test//hello(not matching)
+    //test/*/hello -->  test/1/hello( matching)
+    //test/*/hello -->  test/abcd/hello( matching)
+    //test/*/hello -->  test/abc/da/hello(not matching)
 
     @GetMapping(path = "test/*/hello")
     public String test(){
         return "Get Mapping Invoked (test) ";
     }
+
+    @GetMapping(path = "hello/*/*")
+    public String test2(){
+        return "Get Mapping Invoked (test2) ";
+    }
+
+
 
 }
