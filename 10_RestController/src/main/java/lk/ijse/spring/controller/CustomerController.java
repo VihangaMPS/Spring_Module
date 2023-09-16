03e3e57f1733c4e11bfc69dbda0f57bb7d2d47de
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-16 12:14:17 +0530

    Add Module RestController

--- 10_RestController/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 10_RestController/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,15 +1,30 @@
 package lk.ijse.spring.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
 
-
+    @GetMapping
     public String getAllCustomer(){
         return "Hello There..?";
     }
+    @PostMapping
+    public String saveCustomer(){
+        return "Customer Saved and Post Methods Invoked";
+    }
+
+    @DeleteMapping
+    public String DeleteCustomer(){
+        return "Customer Delete and Delete Method Invoked";
+    }
+
+    @PutMapping
+    public String updatedCustomer(){
+        return "Customer Updated and Customer Methods";
+    }
+
 }
+
