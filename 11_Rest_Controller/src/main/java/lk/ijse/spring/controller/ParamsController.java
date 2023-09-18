17a0098edd5f2a8fbc8aea8e95db7d506963c89b
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-19 00:03:24 +0530

    Updated Content

--- 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/ParamsController.java
+++ 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/ParamsController.java
@@ -2,20 +2,25 @@ package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("params")
 public class ParamsController {
 
-    @GetMapping(params = {"param1","param2"})
-    public String test1(){
-        return "Hello 1";
+    @GetMapping(params = {"id","name"})
+    public String test1(String id,String name){
+        return "Hello 1" + id+" "+name;
     }
-
 
     @GetMapping
     public String test2(){
-        return "Hello 2";
+        return "Hello @2";
+    }
+
+    @GetMapping(params = {"name","salary"})
+    public String test3(String name,@RequestParam("salary") double mySalary){
+        return "Hello 3" +name+" "+mySalary;
     }
 }
