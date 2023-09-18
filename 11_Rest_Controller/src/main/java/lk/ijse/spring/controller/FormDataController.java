9157c143654c5a99ed5fbc47e451597430432e66
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-19 01:36:20 +0530

    Updated Content

--- 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/FormDataController.java
+++ 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/FormDataController.java
@@ -1,14 +1,20 @@
 package lk.ijse.spring.controller;
 
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("from")
+@RequestMapping("form")
 public class FormDataController {
 
-
+    @PostMapping
+    public String testForm1(@ModelAttribute CustomerDTO dto){
+        return "Hello Request Received "+dto.toString();
+    }
 
 
 }
