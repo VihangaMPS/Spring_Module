cc29b11bb2f50b7390775c7f2b861a23b18883f3
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-20 14:54:11 +0530

    intro json Controller

---
+++ 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("json")
+public class JSONController {
+
+}
