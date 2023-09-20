50946ded3a2f9ab87024b39b96dde782f63bc96d
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-20 16:49:44 +0530

    Content updated

--- 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
+++ 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
@@ -1,15 +1,19 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("json")
 public class JSONController {
 
     @PostMapping
-    public String getJsonRequest(){
-        return "Hello JSON";
+    public String getJsonRequest(@ModelAttribute CustomerDTO dto){
+        return "Hello JSON"+ dto.toString();
+    }
+
+    @GetMapping(produces = {"application/json"})
+    public CustomerDTO sendBackJSON( ){
+        return new  CustomerDTO("C001","Amal","Galle",1000.00);
     }
 }
