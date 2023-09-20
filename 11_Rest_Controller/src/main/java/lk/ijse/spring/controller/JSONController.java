1b5d94760a825a92cfb91d855fb9690cd55f87c3
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-20 15:45:47 +0530

    intro json Controller

--- 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
+++ 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("json")
 public class JSONController {
 
+    @PostMapping
+    public String getJsonRequest(){
+        return "Hello JSON";
+    }
 }
