423e611c8288a532dee5f1da9aa34636c478374c
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-21 12:02:46 +0530

    Requset Body

--- 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
+++ 11_Rest_Controller/src/main/java/lk/ijse/spring/controller/JSONController.java
@@ -1,19 +1,27 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("json")
 public class JSONController {
 
-    @PostMapping
-    public String getJsonRequest(@ModelAttribute CustomerDTO dto){
+    @PostMapping //if u want to get json request content u have to use @RequestBody
+    public String getJsonRequest(@RequestBody CustomerDTO dto){
         return "Hello JSON"+ dto.toString();
     }
 
-    @GetMapping(produces = {"application/json"}) //content-type = application/json
-    public CustomerDTO sendBackJSON( ){
-        return new  CustomerDTO("C001","Amal","Galle",1000.00);
+    // to return json response u need json converter in the classpath
+    @GetMapping(path = "array",produces = {MediaType.APPLICATION_JSON_VALUE}) //content-type = application/json
+    public ArrayList<CustomerDTO> sendBackJSON( ){
+        ArrayList<CustomerDTO> all = new ArrayList<>();
+        all.add(new  CustomerDTO("C001","Amal","Galle",1000.00));
+        all.add(new  CustomerDTO("C002","kamal","mathara",5440.00));
+        all.add(new  CustomerDTO("C003","piyal","colombo",979190.00));
+        return all;
     }
 }
