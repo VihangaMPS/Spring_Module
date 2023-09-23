0f1ad3239078c0d3fe11646feb3601fd954b6cb0
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-23 23:28:02 +0530

    added index file

--- 14_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 14_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,18 +2,37 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-        bean.setJpaVendorAdapter(); //vendor
-        bean.setDataSource(); //connection
+        bean.setJpaVendorAdapter(va); //vendor
+        bean.setDataSource(ds); //connection
         bean.setPackagesToScan("lk.ijse.spring.entity"); // location of the entity
         return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springjpa?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+
     }
 
 }
