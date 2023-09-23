a319d51312d22b8355e801a69bb26a9873d1b8c1
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-23 22:56:19 +0530

    Content Updated

--- 14_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 14_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 public class JPAConfig {
 
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        return bean; 
+    }
 
 }
