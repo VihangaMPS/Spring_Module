a9692340eee4e358898b2a3ecedd0479ff47f322
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-23 13:03:37 +0530

    Content Updated

--- 12_SpringWithMave/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 12_SpringWithMave/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,45 +1,21 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("customer")
+import java.util.ArrayList;
+
+@RequestMapping("api/v1/customer")
 public class CustomerController {
 
-//    @GetMapping
-//    public String getAllCustomer(){
-//        return "Hello Maven";
-//    }
-
-    // we can narrow down request using headers also
-
-/*
-
-    // consume
-    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public CustomerDTO searchCustomer(){
-        return new CustomerDTO("C002","CMJD","Galle",45454.00);
-    }
-
-    // produces
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public CustomerDTO getAllCustomer(){
-        return new CustomerDTO("C001","IJSE","Panadura",45454.00);
-    }
-
-*/
-
-    //consume - if we put consumes type that means u have to put content type header
-    //in the request
-
-    //produce - if u want to state that what u'r are going to produce from here u can use produced header
-    //also put to accept header in the request header
-    @GetMapping(consumes = "application/json" ,produces = "application/json")
-    public CustomerDTO testMethodOne(){
-        return new CustomerDTO("C001","IJSE","Panadura",45454.00);
+    @GetMapping
+    public ArrayList<CustomerDTO> getAllCustomer(){
+        ArrayList<CustomerDTO> arrayList = new ArrayList<>();
+        arrayList.add(new CustomerDTO("C001","Dasun","GAlle",154084));
+        arrayList.add(new CustomerDTO("C002","Nimal","Mathara",1000));
+        arrayList.add(new CustomerDTO("C003","piyal","galle",84512));
+        arrayList.add(new CustomerDTO("C004","sunil","colombo",52226));
+        return arrayList;
     }
 }
