474e1bd6dd2daa5d309555923989faa5183f81fa
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-27 19:49:13 +0530

    Introduced how to test a class

--- 14_SpringDataJPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ 14_SpringDataJPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -11,16 +11,17 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebAppConfiguration
-@ContextConfiguration(classes = {JPAConfig.class})
-@ExtendWith(SpringExtension.class)
+@WebAppConfiguration //state test configuration class
+@ContextConfiguration(classes = {JPAConfig.class}) // import configuration for test context
+@ExtendWith(SpringExtension.class) // Run With Spring Extension
 class CustomerRepoTest {
 
     @Autowired
     CustomerRepo customerRepo;
 
-    @Test
+    @Test //test method
     public void savedCustomer(){
+        // Ok let's save a customer using Customer Repo
         Customer customer1 = new Customer("C001","some","panadura",10000.00);
         Customer customer2 = new Customer("C002","piyal","Mathara",25000.00);
         Customer customer3 = new Customer("C003","Gune","Weligama",5000.00);
