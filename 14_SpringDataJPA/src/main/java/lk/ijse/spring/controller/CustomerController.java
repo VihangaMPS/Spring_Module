d8a506e44b8b231aa1598fb2e2eac534f83a568a
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-28 22:20:52 +0530

    Updated Content

--- 14_SpringDataJPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 14_SpringDataJPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customer")
@@ -15,8 +18,15 @@ public class CustomerController {
     @Autowired
     CustomerServiceImpl customerService;
 
-    @GetMapping
+   /* @GetMapping
     public String getAllCustomer(){
         return "helli app is working";
+    }*/
+
+
+    @GetMapping
+    public List<Customer> getAllCustomer(){
+        return customerService.getAllCustomer();
     }
+
 }
