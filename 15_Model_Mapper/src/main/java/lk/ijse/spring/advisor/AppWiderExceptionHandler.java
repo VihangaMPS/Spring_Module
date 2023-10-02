4882990d69f2da2229bca649fdc6f078f8fc7359
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-02 15:34:55 +0530

    Updated Content

---
+++ 15_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWiderExceptionHandler.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.advisor;
+
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@CrossOrigin
+@RestControllerAdvice
+public class AppWiderExceptionHandler {
+
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e){
+        return new  ResponseUtil(500,e.getMessage(),null);
+
+    }
+}
